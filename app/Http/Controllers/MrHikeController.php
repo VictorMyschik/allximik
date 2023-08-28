610b11cdceabd325afd195979e4d0f518fdedfb2
Victor Myschik <vmyschik@quintegro.com>
2023-08-28 21:49:38 +0200

    Redirect after login with token

--- app/Http/Controllers/MrHikeController.php
+++ app/Http/Controllers/MrHikeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Hike;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class MrHikeController extends Controller
 {
@@ -11,7 +12,12 @@ class MrHikeController extends Controller
   {
     abort_if(!is_numeric($token), 404);
 
+    /** @var Hike $hike */
     $hike = Hike::where('public_id', $token)->firstOrFail();
+
+    if (Auth::guest()) {
+      return redirect()->route('login')->withInput(['token' => $hike->getPublicId()]);
+    }
 
     return redirect()->route('hike.details', ['hike' => $hike->id()]);
   }
