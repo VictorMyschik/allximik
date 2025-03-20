9a8d0e1dc733809fefc362a3810837e73a980207
Viktar Myshchyk <v.myshchik@apicworld.com>
2025-03-20 21:16:00 +0100

    Manage bot

--- tests/Feature/MyTest.php
+++ tests/Feature/MyTest.php
@@ -11,17 +11,6 @@ class MyTest extends TestCase
 {
     public function testParse(): void
     {
-        $user = '488545536';
 
-        $ids = DB::table(UserLink::getTableName() . ' as ul1')
-            ->join(UserLink::getTableName() . ' as ul2', 'ul1.link_id', '=', 'ul2.link_id')
-            ->where('ul1.user', $user)
-            ->groupBy('ul1.link_id')
-            ->havingRaw('COUNT(ul2.link_id) = 1')
-            ->pluck('ul1.link_id')
-            ->toArray();
-
-        DB::table(Link::getTableName())->whereIn('id', $ids)->delete();
-        DB::table(UserLink::getTableName())->where('user', $user)->delete();
     }
 }
